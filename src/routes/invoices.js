b7a497062c6eb1cba0ba3a5d45b1a939abb0e3eb
manishrana9 <39648873+manishrana9@users.noreply.github.com>
2021-11-07 13:24:58 -0800

    Initial Commit

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -2,7 +2,7 @@ import { NavLink, Outlet,useSearchParams } from "react-router-dom";
 import { getInvoices } from "../data";
 
 export default function Invoices() {
-  let invoices = getInvoices();
+  let invoices = getInvoices(); 
   let [searchParams, setSearchParams] = useSearchParams();
   return (
     
