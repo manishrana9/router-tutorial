b1282ca402b1f057c6627b78c0153a2a95ff23dc
manishrana9 <39648873+manishrana9@users.noreply.github.com>
2021-11-07 12:57:32 -0800

    react router tutorial

---
+++ src/routes/expenses.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function expenses() {
+    return (
+        <div>
+        <main style={{ padding: "1rem 0" }}>
+      <h2>Expenses</h2>
+    </main>
+    
+        </div>
+    )
+}
+
+export default expenses
